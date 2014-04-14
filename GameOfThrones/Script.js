311da61b38ac5110331b0f3032b72a0452db1ebe
sprasains1 <sabal_prasain@hotmail.com>
2014-04-14 13:23:02 -0700

    angularGAMEOF THRONES INit

---
+++ GameOfThrones/Script.js
@@ -0,0 +1,19 @@
+﻿
+
+
+function PlayerController($scope) {
+   
+    $scope.player = [{ Name: "Lisa Tully", House: "Arryn", Picture: "Lisa's Picture" },
+                        { Name: "Rickon Stark", House: "stark", Picture: "Rickon" },
+    { Name: "Edmure Tully", House: "Tully", Picture: "Edmure" }];
+    $scope.addPlayer = function (name, house, picture) {
+
+        $scope.player.push({ Name: name, House: house, Picture: picture });
+        $scope.Name = "";
+        $scope.House = "";
+        $scope.Picture = "";
+        
+    };
+};
+$('#bt').popover('toggle');
+$('#bt').popover('hide');
